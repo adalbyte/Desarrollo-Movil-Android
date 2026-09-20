e9f865278434d61453a5474af641298c7d77e7bf
Angel <angeladalbertoc59@gmail.com>
2026-09-19 21:06:35 -0600

    Practica 04: Bandera de Alemania con Column y Row

---
+++ app/src/main/java/com/adalbyte/banderascompose/BanderaAlemania.kt
@@ -0,0 +1,42 @@
+package com.adalbyte.banderascompose
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BanderaAlemania(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color(0xFF000000))
+        ) {}
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color(0xFFDD0000))
+
+        ) {}
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color(0xFFFFCE00))
+        ) {}
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BanderaAlemaniaPreview() {
+    Surface {
+        BanderaAlemania(modifier = Modifier.fillMaxSize())
+    }
+}
