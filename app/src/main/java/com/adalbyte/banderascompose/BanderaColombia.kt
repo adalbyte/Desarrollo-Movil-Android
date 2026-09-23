ec42268e3d1003ea744bab25174eb071adbaa8b9
Angel <angeladalbertoc59@gmail.com>
2026-09-23 10:48:50 -0600

    Practica 06: Bandera Colombia con Screens

--- app/src/main/java/com/adalbyte/banderascompose/BanderaColombia.kt
+++ app/src/main/java/com/adalbyte/banderascompose/BanderaColombia.kt
@@ -10,23 +10,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.adalbyte.banderascompose.screens.BanderaScreen
 
 @Composable
 fun BanderaColombia(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxSize()) {
-        Box(modifier = Modifier
-            .weight(2f)
-            .fillMaxWidth()
-            .background(Color(0xFFFFFF00)))
-        Box(modifier = Modifier
-            .weight(1f)
-            .fillMaxWidth()
-            .background(Color(0xFF0000FF)))
-        Box(modifier = Modifier
-            .weight(1f)
-            .fillMaxWidth()
-            .background(Color(0xFFFF0000)))
-    }
+    BanderaScreen();
 }
 
 @Preview(showBackground = true)
