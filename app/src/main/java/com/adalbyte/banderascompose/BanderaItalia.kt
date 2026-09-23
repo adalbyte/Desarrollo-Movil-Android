8f1dbfeea36fab2ea5b5a6dffcf900e964b389d5
Angel <angeladalbertoc59@gmail.com>
2026-09-23 10:33:10 -0600

    Practica 03: Bandera Italia con Screens

--- app/src/main/java/com/adalbyte/banderascompose/BanderaItalia.kt
+++ app/src/main/java/com/adalbyte/banderascompose/BanderaItalia.kt
@@ -8,38 +8,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.adalbyte.banderascompose.screens.BanderaScreen
+import com.adalbyte.banderascompose.screens.BanderaScreenPreview
 
 @Composable
 fun BanderaItalia(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color(0xFF009246))
-        ) {}
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color.White),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-
-        ) {}
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color(0xFFCE2B37))
-        ) {}
-    }
+    BanderaScreen();
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BanderaItaliaPreview() {
-    Surface {
-        BanderaItalia(modifier = Modifier.fillMaxSize())
-    }
+    BanderaScreenPreview();
 }
---
+++ app/src/main/java/com/adalbyte/banderascompose/screens/Bandera.kt
@@ -0,0 +1,53 @@
+package com.adalbyte.banderascompose.screens
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.adalbyte.banderascompose.R
+
+@Composable
+fun BanderaScreen(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color(0xFF009246))
+        ) {}
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color.White),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+        ) {}
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color(0xFFCE2B37))
+        ) {}
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BanderaScreenPreview() {
+    Surface {
+        BanderaScreen(modifier = Modifier.fillMaxSize())
+    }
+}
+
+
