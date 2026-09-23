3b72994eff63d54714bcac459beaecf95adebf3e
KazukiGames82 <saaggosao@gmail.com>
2026-09-23 11:52:43 -0600

    Practica 10: Bandera Estados Unidos

--- app/src/main/java/com/adalbyte/banderascompose/MainActivity.kt
+++ app/src/main/java/com/adalbyte/banderascompose/MainActivity.kt
@@ -8,14 +8,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.adalbyte.banderascompose.screens.BanderaScreen
 
 @Composable
-fun Bandera<NombrePais>(modifier: Modifier = Modifier) {
+fun BanderaEEUU(modifier: Modifier = Modifier) {
     BanderaScreen()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun Bandera<NombrePais>Preview() {
+fun BanderaEEUUPreview() {
     Surface {
-        Bandera<NombrePais>(modifier = Modifier.fillMaxSize())
+        BanderaEEUU(modifier = Modifier.fillMaxSize())
     }
 }
--- app/src/main/java/com/adalbyte/banderascompose/screens/Bandera.kt
+++ app/src/main/java/com/adalbyte/banderascompose/screens/Bandera.kt
@@ -13,11 +13,52 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.adalbyte.banderascompose.R
 
 @Composable
 fun BanderaScreen(modifier: Modifier = Modifier) {
-
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(Modifier.fillMaxSize()) {
+            repeat(13) { index ->
+                Box(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .background(if (index % 2 == 0) Color(0xFFB22234) else Color.White)
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.4f)
+                .fillMaxHeight(0.54f)
+                .background(Color(0xFF3C3B6E))
+        )
+        {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                repeat(5) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        repeat(6) {
+                            Text(
+                                text = "★",
+                                color = Color.White,
+                                fontSize = 24.sp
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
