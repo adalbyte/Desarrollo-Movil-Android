b3bfb85374803586854344854a43e4f02f0cb344
Angel <angeladalbertoc59@gmail.com>
2026-09-23 10:26:16 -0600

    Practica 01: Bandera Mexico con Screens

--- app/src/main/java/com/adalbyte/banderascompose/BanderaMexico.kt
+++ app/src/main/java/com/adalbyte/banderascompose/BanderaMexico.kt
@@ -10,45 +10,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.adalbyte.banderascompose.screens.BanderaScreen
+import com.adalbyte.banderascompose.screens.BanderaScreenPreview
 
 @Composable
 fun BanderaMexico(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color(0xFF006341)) // verde
-        ) {}
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color.White),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.escudo_mexico),
-                contentDescription = "Escudo nacional",
-                modifier = Modifier.size(60.dp)
-            )
-        }
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color(0xFFCE1126)) // rojo
-        ) {}
-    }
+    BanderaScreen();
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BanderaMexicoPreview() {
-    Surface {
-        BanderaMexico(modifier = Modifier.fillMaxSize())
-    }
+    BanderaScreenPreview();
 }
 
---
+++ app/src/main/java/com/adalbyte/banderascompose/screens/Bandera.kt
@@ -0,0 +1,61 @@
+package com.adalbyte.banderascompose.screens
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.adalbyte.banderascompose.R
+
+@Composable
+fun BanderaScreen(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color(0xFF006341)) // verde
+        ) {
+            Text(text = stringResource(id = R.string.app_name))
+        }
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color.White),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.escudo_mexico),
+                contentDescription = "Escudo nacional",
+                modifier = Modifier.size(60.dp)
+            )
+        }
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(colorResource(id = R.color.RojoM)) // rojo
+        ) {}
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BanderaScreenPreview() {
+    Surface {
+        BanderaScreen(modifier = Modifier.fillMaxSize())
+    }
+}
+
+
