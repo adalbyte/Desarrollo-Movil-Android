d76cd8f067499ba5b01fee1ea99b14c9f89c7d3e
Angel <angeladalbertoc59@gmail.com>
2026-09-23 10:13:39 -0600

    Practica 09: Bandera de Japon con Box

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
---
+++ app/src/main/java/com/adalbyte/banderascompose/BanderaJapon.kt
@@ -0,0 +1,38 @@
+package com.adalbyte.banderascompose
+
+import android.text.Layout
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BanderaJapon(modifier: Modifier = Modifier) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(colorResource(id = R.color.white)))
+    {
+        Box(modifier = Modifier
+            .align(alignment = Alignment.Center)
+            .size(200.dp)
+            .clip(CircleShape)
+            .background(Color(0xFFFF0000)))
+        {}
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BanderaJaponPreview() {
+    Surface {
+        BanderaJapon(modifier = Modifier.fillMaxSize())
+    }
+}
