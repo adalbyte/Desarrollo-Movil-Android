8d0c37d27bef514bcf109d5bd70acc1d675e30a9
Angel <angeladalbertoc59@gmail.com>
2026-09-23 10:30:24 -0600

    Practica 02: Bandera Francia con Screens

--- app/src/main/java/com/adalbyte/banderascompose/BanderaFrancia.kt
+++ app/src/main/java/com/adalbyte/banderascompose/BanderaFrancia.kt
@@ -8,38 +8,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.adalbyte.banderascompose.screens.BanderaScreen
+import com.adalbyte.banderascompose.screens.BanderaScreenPreview
 
 @Composable
 fun BanderaFrancia(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color(0xFF002654))
-        ) {}
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color.White),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-
-        ) {}
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color(0xFFCD1125))
-        ) {}
-    }
+    BanderaScreen();
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BanderaFranciaPreview() {
-    Surface {
-        BanderaFrancia(modifier = Modifier.fillMaxSize())
-    }
+    BanderaScreenPreview();
 }
