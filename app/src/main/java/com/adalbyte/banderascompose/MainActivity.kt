02e64742469edcb1a749132eaa3985cc75a65d87
Angel <angeladalbertoc59@gmail.com>
2026-09-23 11:34:25 -0600

    Practica 07: Bandera Argentina

--- app/src/main/java/com/adalbyte/banderascompose/MainActivity.kt
+++ app/src/main/java/com/adalbyte/banderascompose/MainActivity.kt
@@ -8,14 +8,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.adalbyte.banderascompose.screens.BanderaScreen
 
 @Composable
-fun Bandera<NombrePais>(modifier: Modifier = Modifier) {
+fun BanderaArgentina(modifier: Modifier = Modifier) {
     BanderaScreen()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun Bandera<NombrePais>Preview() {
+fun BanderaArgentinaPreview() {
     Surface {
-        Bandera<NombrePais>(modifier = Modifier.fillMaxSize())
+        BanderaArgentina(modifier = Modifier.fillMaxSize())
     }
 }
--- app/src/main/java/com/adalbyte/banderascompose/screens/Bandera.kt
+++ app/src/main/java/com/adalbyte/banderascompose/screens/Bandera.kt
@@ -2,11 +2,13 @@ package com.adalbyte.banderascompose.screens
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -17,7 +19,20 @@ import com.adalbyte.banderascompose.R
 
 @Composable
 fun BanderaScreen(modifier: Modifier = Modifier) {
-
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(Modifier.fillMaxSize()) {
+            Box(Modifier.weight(1f).fillMaxWidth().background(Color(0xFF74ACDF)))
+            Box(Modifier.weight(1f).fillMaxWidth().background(Color.White))
+            Box(Modifier.weight(1f).fillMaxWidth().background(Color(0xFF74ACDF)))
+        }
+        Box(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .size(50.dp)
+                .clip(CircleShape)
+                .background(Color(0xFFF6B40E))
+        )
+    }
 }
 
 @Preview(showBackground = true)
